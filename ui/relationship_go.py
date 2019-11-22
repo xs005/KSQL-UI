b4cbb7ed466a9dce836d11b561fee3488d6050ec
Chris Sun <csun@gmcps.com>
2019-11-22 07:18:24 -0600

    change the citation to avoid duplicated query from the ksql server

--- ui/relationship_go.py
+++ ui/relationship_go.py
@@ -4,28 +4,25 @@ from textwrap import dedent as d
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table
 
 from backend.common import STYLES
-from backend.entities.Stream import Stream
-from backend.rest import REST
 from backend.sankey_diagram import sankey_diagram, stream_list, table_list, link_labels
+from backend.utils import generate_datatable
+from ui import stream_page, table_page, query_page, topic_page
 
 go_components = [
     dcc.Graph(
         id='basic-interactions',
         figure=sankey_diagram,
     ),
-    html.Div([
-        dcc.Markdown(d("""
+    html.Div([html.Div(id='click-data'),
+              dcc.Markdown(d("""
                 **Hover Data**
 
                 Mouse over values in the graph.
             """)),
-        html.Pre(id='hover-data', style=STYLES['pre'])
-    ], className='three columns'),
-
-    html.Div(id='click-data')
+              html.Pre(id='hover-data', style=STYLES['pre'])
+              ], className='three columns'),
 ]
 
 go_display_hover_data_callback_output = dash.dependencies.Output('hover-data', 'children')
@@ -41,34 +38,23 @@ def go_display_hover_data_function(hoverData):
 def go_display_click_data_function(clickData):
     try:
         node_or_link = clickData['points'][0]['label']
-        rest = REST()
         if node_or_link in stream_list:
-            click_df = Stream(rest.get_streams()).df
+            click_df = stream_page.df
             df = click_df.loc[click_df.name == node_or_link, :]
-            return dash_table.DataTable(
-                id='stream_table',
-                columns=[{"name": i, "id": i} for i in df.columns],
-                data=df.to_dict('records'))
+            return generate_datatable(df, 'click_table')
         elif node_or_link in table_list:
-            click_df = Stream(rest.get_tables()).df
+            click_df = table_page.df
             df = click_df.loc[click_df.name == node_or_link, :]
-            return dash_table.DataTable(
-                id='table_table',
-                columns=[{"name": i, "id": i} for i in df.columns],
-                data=df.to_dict('records'))
-        elif node_or_link in link_labels:
-            click_df = Stream(rest.get_queries()).df
+            return generate_datatable(df, 'click_table')
+        elif node_or_link in link_labels and node_or_link.split(' ')[0].upper() not in ['SOURCE', 'SINK']:
+            click_df = query_page.df
             df = click_df.loc[click_df.id == node_or_link, :]
-            return dash_table.DataTable(
-                id='query_table',
-                columns=[{"name": i, "id": i} for i in df.columns],
-                data=df.to_dict('records'))
+            return generate_datatable(df, 'click_table')
+        elif node_or_link.split(' ')[0].upper() in ['SOURCE', 'SINK']:
+            return node_or_link
         else:  # this is topic
-            click_df = Stream(rest.get_topics()).df
+            click_df = topic_page.df
             df = click_df.loc[click_df.name == node_or_link, :]
-            return dash_table.DataTable(
-                id='stream_table',
-                columns=[{"name": i, "id": i} for i in df.columns],
-                data=df.to_dict('records'))
+            return generate_datatable(df, 'click_table')
     except:
         return 'Please click the nodes or links.'
